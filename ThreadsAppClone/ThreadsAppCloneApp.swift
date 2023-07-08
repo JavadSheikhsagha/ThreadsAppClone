b14452f8fa76a8b51f4c7eb423b18d3b8c9ec68b
Javad <sir.hxt@gmail.com>
2023-07-08 12:00:39 +0330

    Initial Commit

---
+++ ThreadsAppClone/ThreadsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsAppCloneApp.swift
+//  ThreadsAppClone
+//
+//  Created by enigma 1 on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
